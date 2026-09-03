5244f127bfb904affb324760cb8389c2f852e7d8
talsitelkol <tal_stilkol@hotmail.com>
2026-09-03 09:35:46 +0300

    RSH-033: restore vehicle.ts and wire launchAccel + V100 gear guard

--- src/game/vehicle.ts
+++ src/game/vehicle.ts
@@ -1,1 +1,17 @@
-PLACEHOLDER
+import { clamp, expSmooth, wrapPi, forwardDelta } from "./math";
+import {
+  absModulate,
+  brakeForce,
+  DEFAULT_ASSISTS,
+  escYaw,
+  HANDLING,
+  hydroplane,
+  launchAccel,
+  pacejka,
+  SURFACE_SPEC,
+  tcsModulate,
+  V100_MPS,
+  WEATHER_SPEC,
+  type AssistFlags,
+  type HandlingMode,
+} from "./physics";
